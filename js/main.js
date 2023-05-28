fb596de54cd8dd051b0ce348d316ef7f2487ab20
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-05-28 16:43:50 -0400

    actualmente lee el formulario del array 2, pero no avanza el texto

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-// Segunda pre Entrega Gabriel Lagrotteria RPG DE TEXTO para CoderHouse
+// Tercera pre Entrega Gabriel Lagrotteria RPG DE TEXTO para CoderHouse
 
-  //Objeto personaje a usar
+//Objeto personaje a usar
 
 // Objeto  Enemigos
 
@@ -31,78 +31,90 @@ const personaje = {
     ataque: 1
 }
 
-  // funcion que Selecciona el nombre
-
-  function seleccionNombre(){
-  formularioNombre = document.getElementById("nombrePj");
-
-
-  formularioNombre.addEventListener("submit", (e) => {
-          e.preventDefault();
-      let nombre = e.target.querySelector("input[type=text]").value;
-      console.log(nombre);
-  });
-
-};
-
- 
-
-
 // Historia en alertas (Se agregan arrays los cuales seran necesarios en la historia)
 function historia() {
 
-    const arrayTextos = [
-        {
+    const arrayTextos = [{
             texto: 1,
             codigo: "<p>¿Podria decirme su nombre por favor?</p>"
         },
         {
             texto: 2,
-            codigo: '<form id="nombrePj"><input type="text"> <input type="submit" value"enviar"></form>',
-        },{
+            codigo: '<form id="nombrePersonaje"><input type="text"> <input type="submit" value="enviar"></form>',
+        }, {
             texto: 3,
             codigo: "<p>Excelente" + nombrePj + "mi nombre es Kaine no se si te haz enterado pero aqui han pasado muchas cosas, un Dragon ha estado atacando esta aldea y necesitamos de un valiente guerrero para que nos ayude a derrotarlo, actualmente no contamos con nadie que sepa utilzar un arma, pero veo que tu cargas una, quizas puedas ayudarnos derrotando unos slimes que se encuentran en el pueblo</p>",
         }
     ]
+
+    // funcion que Selecciona el nombre
     
+    let formularioNombre = document.getElementById("nombrePersonaje");
+
+    let escucharForm = (e) => { 
+        e.preventDefault();
+        let nombre = e.target.children[0].value;
+        nombrePj = nombre;
+        console.log(nombre);
+        textoVariable();
+    }
+
+// Agrega click y varra espaciadora
+
+const barraEspaciadora = (e) => {
+    if (e.key === " ") {
+        textoVariable();
+    }
+};
+
+    function agregarFunciones(){
+        // Este lo cambia al hacer click llamando la funcion arriba
+        textoCambiado.addEventListener("click", textoVariable);
+    
+        // Definicion barra espaciadora como constante asi la puedo llamar a necesidad
+    
+        
+        window.addEventListener("keydown", barraEspaciadora);
+    
+    };
+
+    //Remueve Click y barra espaciadora
+    
+        function removerFunciones(){
+            textoCambiado.removeEventListener("click", textoVariable);
+                window.removeEventListener("keydown", barraEspaciadora);
+        }
+    
+        // evento para que se pueda apretar barra espaciadora aparte del click
+    
+
     // llama al ID y del DOM
 
     let textoCambiado = document.getElementById("textoCambiar");
-    
 
     // Este Codigo de aqui hacia abajo cambia el texto
 
     let principioTexto = 0;
 
     function textoVariable() {
-        if(principioTexto < arrayTextos.length) {
+        if (principioTexto < arrayTextos.length) {
             arrayTextos[principioTexto].codigo;
             textoCambiado.innerHTML = arrayTextos[principioTexto].codigo
             principioTexto++;
-        }else if(principioTexto === 2){ // se agrego un if que agrega un form en el momento indicado
-            const CrearForm = document.createElement("form");
-            CrearForm.id = "nombrePj";
-            CrearForm.innerHTML = '<input type="text"> <input type="submit" value="Enviar">';
-            document.body.appendChild(CrearForm);
-            seleccionNombre();
+            console.log(principioTexto);
+        }
+        if (principioTexto === 2) {
+            removerFunciones();
+            textoCambiado.addEventListener("submit", escucharForm);
+            principioTexto++;
+            agregarFunciones();
+        }
     };
 
-    // Este lo cambia al hacer click llamando la funcion arriba
+    agregarFunciones();
 
-    textoCambiado.addEventListener("click", textoVariable);
-        
-    // evento para que se pueda apretar barra espaciadora aparte del click
+}; 
 
-
- window.addEventListener("keydown", (e) =>{
-        if(e.key === " ") {
-            textoVariable();
-        }
-    });
-}
-
-
-}
-
-
-historia()
+document.addEventListener("DOMContentLoaded", function () {
+    historia()
+});
